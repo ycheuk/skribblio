5555558b4d65218a80808fd1aecc412614e3eb57
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-09 21:52:48 -0800

    Update app.js
    added comments, new events for the canvas

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
+// importing all the important node.js/express/etc. stuff
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const path = require('path');
 
+// now setting up socket.io
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+// doing this so the html can access the stylesheet/client-facing code
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use index.html as the 'home page'
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// when a user connects/opens localhost:8080
 io.on('connection', (socket) => {
-    console.log('a new user connected');
 
-    // socket.on('event', () => {});
+    // when something is drawn on the canvas
+    socket.on('drawn', (data) => {
+        io.emit('drawn', data); // just sending the data back
+    });
+
+    // when a user presses the 'clear' button
+    socket.on('cleared', () => {
+        io.emit('cleared');
+    });
 
 });
 
+// getting the server up n running!
 server.listen(8080, () => {
     console.log('listening on port 8080');
 });
