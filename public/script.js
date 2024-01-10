10cf1ebec4d731f75cd8422d90ff02b8cf85fcb0
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2024-01-09 19:15:47 -0800

    Update script.js
    canvas is functional! no socket.io implementation tho

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ let currentWord; // stores the current word chosen to be drawn
 // starts a new round
 function startRound() {
   currentWord = getRandomWord();
-  clearCanvas();
+  // clearCanvas();
 }
 
 // get a random word from wordList
@@ -28,9 +28,75 @@ function sendMessage() {
 
 }
 
-// drawing on canvas
-function drawOnCanvas() {
+startRound()
 
+/* 
+
+
+space
+
+hehe
+
+
+*/
+
+var myCanvas = document.querySelector('canvas');
+var myContext = myCanvas.getContext('2d');
+
+myCanvas.height = .8 * window.innerHeight;
+myCanvas.width = .8 * window.innerWidth;
+
+let drawing = false;
+
+let posX = 0;
+let posY = 0;
+
+myCanvas.addEventListener('mousedown', (e) => {
+  init(e);
+  drawing = true;
+});
+
+myCanvas.addEventListener('mouseup', () => {
+  drawing = false;
+
+});
+
+myCanvas.addEventListener('mousemove', (e) => {
+  draw(e);
+});
+
+function init(e) {
+  posX = e.offsetX;
+  posY = e.offsetY;
 }
 
-startRound()
+function draw(e) {
+  if (drawing) {
+    myContext.beginPath();
+
+    myContext.moveTo(posX, posY);
+    myContext.lineTo(e.offsetX, e.offsetY);
+    myContext.stroke();
+
+    myContext.closePath();
+
+    init(e);
+  }
+}
+
+// color btns
+const blackBtn = document.getElementById('black');
+const pinkBtn = document.getElementById('pink');
+const clearBtn = document.getElementById('clear');
+
+clearBtn.addEventListener('click', () => {
+  myContext.clearRect(0,0,myCanvas.width,myCanvas.height);
+});
+
+blackBtn.addEventListener('click', () => {
+  myContext.strokeStyle = '#000000';
+});
+
+pinkBtn.addEventListener('click', () => {
+  myContext.strokeStyle = '#ffa6c9';
+});
