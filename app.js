9dab6dbc1251eefb1ddee555dde20e2c2e9e5d73
Yik Yin Cheuk <ycheuk@Yiks-MacBook.local>
2024-01-10 12:15:37 -0500

    testing

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-// importing all the important node.js/express/etc. stuff
 const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const path = require('path');
-
-// now setting up socket.io
+const { createServer } = require('http');
+const { join } = require('path');
 const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
 const io = new Server(server);
 
 // doing this so the html can access the stylesheet/client-facing code
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
 
 // use index.html as the 'home page'
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-// when a user connects/opens localhost:8080
+// connect
 io.on('connection', (socket) => {
+  console.log('a user connected');
 
-    // when something is drawn on the canvas
-    socket.on('drawn', (data) => {
-        io.emit('drawn', data); // just sending the data back
-    });
+  // listen for user's name
+  socket.on('sendName', (name) => {
+    socket.username = name; // store the user's name in the socket object
+  });
 
-    // when a user presses the 'clear' button
-    socket.on('cleared', () => {
-        io.emit('cleared');
-    });
+  // listen for chat messages
+  socket.on('chat message', (data) => {
+    console.log(`${socket.username} said: ${data.message}`);
+    io.emit('chat message', { name: socket.username, message: data.message });
+  });
 
+  // disconnect
+  socket.on('disconnect', () => {
+    console.log(`${socket.username} disconnected`);
+  });
+
+  // when something is drawn on the canvas
+  socket.on('drawn', (data) => {
+      io.emit('drawn', data); // just sending the data back
+  });
+
+  // when a user presses the 'clear' button
+  socket.on('cleared', () => {
+      io.emit('cleared');
+  });
 });
 
-// getting the server up n running!
-server.listen(8080, () => {
-    console.log('listening on port 8080');
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
 });
